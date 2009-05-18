50d77bca705b8982a7008b02f5272823e1377204
Eung-ju PARK <eungju at gmail dot com>
2009-05-18 17:02:20 +0900

    Fix broken commit.

---
+++ undercover/src/main/java/undercover/instrument/OfflineInstrument.java
@@ -0,0 +1,91 @@
+package undercover.instrument;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import undercover.UndercoverSettings;
+
+public class OfflineInstrument {
+	private Instrument instrument;
+	private File[] inputPaths;
+	private File outputDirectory;
+	private File metaDataFile;
+	private File coverageDataFile;
+	
+	public void setInputPaths(File[] inputPaths) {
+		this.inputPaths = inputPaths;
+	}
+	
+	public void setOutputDirectory(File outputDirectory) {
+		this.outputDirectory = outputDirectory;
+	}
+	
+	public void setMetaDataFile(File metaDataFile) {
+		this.metaDataFile = metaDataFile;
+	}
+
+	public void setCoverageDataFile(File coverageDataFile) {
+		this.coverageDataFile = coverageDataFile;
+	}
+	
+	public void run() throws Exception {
+		instrument = new Instrument();
+		instrumentDirs(inputPaths, outputDirectory);
+		instrument.getMetaData().save(metaDataFile);
+    	
+		UndercoverSettings settings = new UndercoverSettings();
+		settings.setCoverageSaveOnExit(true);
+		settings.setCoverageFile(coverageDataFile);
+		settings.save(new File(outputDirectory, "undercover.properties"));
+	}
+	
+	public void instrumentDirs(File[] inputDirs, File outputDir) {
+		outputDir.mkdirs();
+		for (File each : inputDirs) {
+			instrumentDir(each, outputDir);
+		}
+	}
+	
+	public void instrumentDir(File inputDir, File outputDir) {
+		outputDir.mkdir();
+		for (File each : inputDir.listFiles()) {
+			if (each.isDirectory()) {
+				instrumentDir(each, new File(outputDir, each.getName()));
+			} else if (each.getName().endsWith(".class")) {
+				instrumentFile(each, new File(outputDir, each.getName()));
+			} else {
+				copyFile(each, new File(outputDir, each.getName()));
+			}
+		}
+	}
+
+	void copyFile(File inputFile, File outputFile) {
+		try {
+			FileUtils.copyFile(inputFile, outputFile);
+		} catch (IOException e) {
+			throw new InstrumentException("Cannot copy " + inputFile.getAbsolutePath() + " to " + outputFile.getAbsoluteFile(), e);
+		}
+	}
+
+	public void instrumentFile(File inputFile, File outputFile) {
+		InputStream input = null;
+		OutputStream output = null;
+		try {
+			input = new FileInputStream(inputFile);
+			output = new FileOutputStream(outputFile);
+			output.write(instrument.instrument(input));
+		} catch (IOException e) {
+			throw new InstrumentException("Cannot instrument " + inputFile.getAbsolutePath() + " to " + outputFile.getAbsoluteFile(), e);
+		} finally {
+			IOUtils.closeQuietly(output);
+			IOUtils.closeQuietly(input);
+		}
+	}
+}
