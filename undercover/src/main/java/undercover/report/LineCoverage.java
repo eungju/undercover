d766f7450013309a861610d215356a8f76d46949
Eung-ju PARK <eungju at google dot com>
2009-05-20 13:11:34 +0900

    Show touch count.

--- undercover/src/main/java/undercover/report/LineCoverage.java
+++ undercover/src/main/java/undercover/report/LineCoverage.java
@@ -5,11 +5,13 @@ import undercover.metric.BlockCoverage;
 public class LineCoverage {
 	public int blockCount = 0;
 	public int coveredBlockCount = 0;
+	public int touchCount = 0;
 
 	public void addBlock(BlockCoverage blockCoverage) {
 		blockCount++;
 		if (blockCoverage.isTouched()) {
 			coveredBlockCount++;
+			touchCount  = Math.max(touchCount, blockCoverage.touchCount());
 		}
 	}
 	
