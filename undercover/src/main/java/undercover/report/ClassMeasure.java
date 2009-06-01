873457492f6e40679edc27815d13c006015cd9ed
Eung-ju PARK <eungju at gmail dot com>
2009-06-02 00:21:28 +0900

    Make them top level.

--- undercover/src/main/java/undercover/report/ClassMeasure.java
+++ undercover/src/main/java/undercover/report/ClassMeasure.java
@@ -1,8 +1,6 @@
 package undercover.report;
 
-import java.util.Collection;
 
-import undercover.support.LazyValue;
 
 public abstract class ClassMeasure extends MethodMeasure {
 	public abstract int getMethodCount();
@@ -10,95 +8,5 @@ public abstract class ClassMeasure extends MethodMeasure {
 	
 	public double getAverageMethodComplexity() {
 		return ((double) getComplexity()) / getMethodCount();
-	}
-	
-	static class LazyComplexity extends LazyValue<Integer> {
-		private Collection<? extends MethodMeasure> children;
-
-		public LazyComplexity(Collection<? extends MethodMeasure> children) {
-			this.children = children;
-		}
-		
-		protected Integer calculate() {
-			int result = 0;
-			for (MethodMeasure each : children) {
-				result += each.getComplexity();
-			}
-			return result;
-		}
-	};
-	
-	static class LazyBlockCount extends LazyValue<Integer> {
-		private Collection<? extends MethodMeasure> children;
-
-		public LazyBlockCount(Collection<? extends MethodMeasure> children) {
-			this.children = children;
-		}
-		
-		protected Integer calculate() {
-			int result = 0;
-			for (MethodMeasure each : children) {
-				result += each.getBlockCount();
-			}
-			return result;
-		}
-	};
-	
-	static class LazyCoveredBlockCount extends LazyValue<Integer> {
-		private Collection<? extends MethodMeasure> children;
-
-		public LazyCoveredBlockCount(Collection<? extends MethodMeasure> children) {
-			this.children = children;
-		}
-		
-		protected Integer calculate() {
-			int result = 0;
-			for (MethodMeasure each : children) {
-				result += each.getCoveredBlockCount();
-			}
-			return result;
-		}
-	};
-
-	static class LazyMethodCount extends LazyValue<Integer> {
-		private Collection<? extends MethodMeasure> children;
-
-		public LazyMethodCount(Collection<? extends MethodMeasure> children) {
-			this.children = children;
-		}
-		
-		protected Integer calculate() {
-			int result = 0;
-			for (MethodMeasure each : children) {
-				if (each instanceof ClassMeasure) {
-					result += ((ClassMeasure) each).getMethodCount();
-				} else {
-					result += 1;
-				}
-			}
-			return result;
-		}
-	};
-
-	static class LazyMaximumMethodComplexity extends LazyValue<Integer> {
-		private Collection<? extends MethodMeasure> children;
-
-		public LazyMaximumMethodComplexity(Collection<? extends MethodMeasure> children) {
-			this.children = children;
-		}
-		
-		protected Integer calculate() {
-			int result = 0;
-			for (MethodMeasure each : children) {
-				int complexity;
-				if (each instanceof ClassMeasure) {
-					complexity = ((ClassMeasure) each).getMaximumMethodComplexity();
-				} else {
-					complexity = each.getComplexity();
-				}
-				result = Math.max(result, complexity);
-			}
-			return result;
-		}
 	};
 }
