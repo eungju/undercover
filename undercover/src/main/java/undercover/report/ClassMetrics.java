ddaaee652cb4fa713cb71430f465b215f1930243
Eung-ju PARK <eungju at gmail dot com>
2009-06-08 22:23:06 +0900

    Add maximum complexity to class metrics and package metrics.

--- undercover/src/main/java/undercover/report/ClassMetrics.java
+++ undercover/src/main/java/undercover/report/ClassMetrics.java
@@ -2,17 +2,39 @@ package undercover.report;
 
 import java.util.Collection;
 
+import undercover.support.LazyValue;
+
 public class ClassMetrics {
 	private final LazyClassCount classCount;
 	private final BlockMetrics blockMetrics;
+	private final LazyValue<Integer> maximumComplexity;
 
-	public ClassMetrics(Collection<? extends Item> children, BlockMetrics blockMetrics) {
+	public ClassMetrics(final Collection<? extends Item> children, BlockMetrics blockMetrics) {
 		classCount = new LazyClassCount(children);
 		this.blockMetrics = blockMetrics;
+		maximumComplexity = new LazyValue<Integer>() {
+			protected Integer calculate() {
+				int result = 0;
+				for (Item each : children) {
+					int complexity;
+					if (each instanceof ClassItem) {
+						complexity = each.getBlockMetrics().getComplexity();
+					} else {
+						complexity = each.getClassMetrics().getMaximumComplexity();
+					}
+					result = Math.max(result, complexity);
+				}
+				return result;
+			}
+		};
 	}
 	
 	public int getCount() {
 		return classCount.value();
+	}
+	
+	public int getMaximumComplexity() {
+		return maximumComplexity.value();
 	}
 
 	public double getAverageComplexity() {
--- undercover/src/main/java/undercover/report/PackageMetrics.java
+++ undercover/src/main/java/undercover/report/PackageMetrics.java
@@ -2,17 +2,39 @@ package undercover.report;
 
 import java.util.Collection;
 
+import undercover.support.LazyValue;
+
 public class PackageMetrics  {
 	private final LazyPackageCount packageCount;
-	private BlockMetrics blockMetrics;
+	private final BlockMetrics blockMetrics;
+	private final LazyValue<Integer> maximumComplexity;
 
-	public PackageMetrics(Collection<? extends Item> children, BlockMetrics blockMetrics) {
+	public PackageMetrics(final Collection<? extends Item> children, BlockMetrics blockMetrics) {
 		packageCount = new LazyPackageCount(children);
 		this.blockMetrics = blockMetrics;
+		maximumComplexity = new LazyValue<Integer>() {
+			protected Integer calculate() {
+				int result = 0;
+				for (Item each : children) {
+					int complexity;
+					if (each instanceof PackageItem) {
+						complexity = each.getBlockMetrics().getComplexity();
+					} else {
+						complexity = each.getPackageMetrics().getMaximumComplexity();
+					}
+					result = Math.max(result, complexity);
+				}
+				return result;
+			}
+		};
 	}
-	
+
 	public int getCount() {
 		return packageCount.value();
+	}
+	
+	public int getMaximumComplexity() {
+		return maximumComplexity.value();
 	}
 
 	public double getAverageComplexity() {
