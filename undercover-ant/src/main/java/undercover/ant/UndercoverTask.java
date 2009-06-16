31a5f21e152a645c5b5a66ebba2d31bba119f7b9
Eung-ju PARK <eungju at google dot com>
2009-06-16 09:59:47 +0900

    Created Ant module.

---
+++ undercover-ant/src/main/java/undercover/ant/UndercoverTask.java
@@ -0,0 +1,16 @@
+package undercover.ant;
+
+import org.apache.tools.ant.Task;
+
+public class UndercoverTask extends Task {
+    public void execute() {
+        // use of the reference to Project-instance
+        String message = getProject().getProperty("ant.project.name");
+
+        // Task's log method
+        log("Here is project '" + message + "'.");
+
+        // where this task is used?
+        log("I am used in: " +  getLocation() );
+    }
+}
