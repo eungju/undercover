76d0d0983942446f80b949f06991b4f5b297e6ef
Eung-ju PARK <eungju at gmail dot com>
2009-07-26 00:31:40 +0900

    Removed debug code.

--- undercover/src/main/java/undercover/report/html/HtmlReport.java
+++ undercover/src/main/java/undercover/report/html/HtmlReport.java
@@ -44,12 +44,10 @@ public class HtmlReport {
 	
 	public void generate() throws IOException {
 		copyResources();
-		long start = System.currentTimeMillis();
 		generateProjectReport();
 		generatePackageReports();
 		generateSourceReports();
 		generateDashboardReport();
-		System.out.println(System.currentTimeMillis() - start);
 	}
 
 	void copyResources() throws IOException {
