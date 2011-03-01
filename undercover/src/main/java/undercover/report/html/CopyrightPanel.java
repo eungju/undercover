c7f291efa47186439500b3d6e937adfecb3f6fc4
Eungju PARK <eungju at gmail dot com>
2011-03-01 20:17:56 +0900

    Fix typo.

--- undercover/src/main/java/undercover/report/html/CopyrightPanel.java
+++ undercover/src/main/java/undercover/report/html/CopyrightPanel.java
@@ -1,15 +1,16 @@
 package undercover.report.html;
 
-import static undercover.report.html.HtmlElements.*;
 import undercover.support.xml.Element;
 import undercover.support.xml.Text;
+
+import static undercover.report.html.HtmlElements.*;
 
 public class CopyrightPanel implements HtmlFragment {
 	public Element build() {
 		return div().attr("class", "copyright").append(
 				p().append(
 						a().attr("href", "http://code.google.com/p/undercover/").attr("target", "_top").append("Undercover"),
-						new Text(" | Copyright © 2009 Eung-ju PARK. All rights reserved. Licensed under the "),
+						new Text(" | Copyright Â© 2009 Eung-ju PARK. All rights reserved. Licensed under the "),
 						a().attr("href", "http://www.apache.org/licenses/LICENSE-2.0").attr("target", "_top").append("Apache License, Version 2.0")
 						)
 				);
