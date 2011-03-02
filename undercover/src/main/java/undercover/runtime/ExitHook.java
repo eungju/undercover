6d2c72bce4f7f544b468c0ec3bc3ecc45fbc2365
Eungju PARK <eungju at gmail dot com>
2011-03-02 21:43:17 +0900

    Encapsulate the Probe.

--- undercover/src/main/java/undercover/runtime/ExitHook.java
+++ undercover/src/main/java/undercover/runtime/ExitHook.java
@@ -1,24 +1,13 @@
 package undercover.runtime;
 
-import java.io.IOException;
-
 public class ExitHook extends Thread {
-	private Probe probe;
-	private UndercoverSettings settings;
-	
+	private final Probe probe;
+
 	public ExitHook(Probe probe) {
 		this.probe = probe;
-		this.settings = probe.getSettings();
 	}
 	
 	public void run() {
-		if (settings.isCoverageSaveOnExit()) {
-			try {
-				CoverageData coverageData = probe.getCoverageData();
-				coverageData.save(settings.getCoverageFile());
-			} catch (IOException e) {
-				throw new RuntimeException("Unable to save coverage data to " + settings.getCoverageFile());
-			}
-		}
+        probe.onExit();
 	}
 }
--- undercover/src/main/java/undercover/runtime/Probe.java
+++ undercover/src/main/java/undercover/runtime/Probe.java
@@ -1,10 +1,12 @@
 package undercover.runtime;
 
+import java.io.IOException;
+
 public class Probe {
 	public final static Probe INSTANCE = new Probe(UndercoverSettings.load());
 	
-	private UndercoverSettings settings;
-	private CoverageData coverageData;
+	private final UndercoverSettings settings;
+	private final CoverageData coverageData;
 	private ExitHook exitHook;
 	
 	public Probe(UndercoverSettings settings) {
@@ -12,27 +14,41 @@ public class Probe {
 		coverageData = new CoverageData();
 		installExitHook();
 	}
-	
-	public UndercoverSettings getSettings() {
-		return settings;
-	}
-	
-	public void installExitHook() {
-		exitHook = new ExitHook(this);
-		Runtime.getRuntime().addShutdownHook(exitHook);
-	}
-	
-	public void uninstallExitHook() {
-		if (exitHook != null) {
-			Runtime.getRuntime().removeShutdownHook(exitHook);
-		}
-	}
-	
+
+    public UndercoverSettings getSettings() {
+        return settings;
+    }
+
+    public void register(String className, int[][] coverage) {
+        coverageData.register(className, coverage);
+    }
+
 	public CoverageData getCoverageData() {
 		return coverageData;
 	}
 
-	public void register(String className, int[][] coverage) {
-		coverageData.register(className, coverage);
-	}
+    public void saveCoverageData() {
+        try {
+            coverageData.save(settings.getCoverageFile());
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to save coverage data to " + settings.getCoverageFile());
+        }
+    }
+
+    public void onExit() {
+        if (settings.isCoverageSaveOnExit()) {
+            saveCoverageData();
+        }
+    }
+
+    public void installExitHook() {
+        exitHook = new ExitHook(this);
+        Runtime.getRuntime().addShutdownHook(exitHook);
+    }
+
+    public void uninstallExitHook() {
+        if (exitHook != null) {
+            Runtime.getRuntime().removeShutdownHook(exitHook);
+        }
+    }
 }
